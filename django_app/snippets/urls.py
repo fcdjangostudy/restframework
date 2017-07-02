97435cfe985beb423ce86f3a5ecd3155c4962abf
GAIUS8 <gaius827@gmail.com>
2017-07-02 18:20:21 +0900

    Create views.py - JSONResponse

--- django_app/snippets/urls.py
+++ django_app/snippets/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 
+from . import views
 
 urlpatterns = [
-
+    url(r'^$', views.JSONResponse.snippet_list),
+    url(r'^/(?P<pk>\d+)$', views.JSONResponse.snippet_detail),
 ]
