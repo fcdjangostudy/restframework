ace5dc1e83375cb32bd9d73addb9647ebcbd5ccc
GAIUS8 <gaius827@gmail.com>
2017-07-02 19:08:35 +0900

    함수기반 뷰 작성

--- django_app/snippets/urls.py
+++ django_app/snippets/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.JSONResponse.snippet_list),
-    url(r'^/(?P<pk>\d+)$', views.JSONResponse.snippet_detail),
+    url(r'^$', views.snippet_list),
+    url(r'^(?P<pk>\d+)/$', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
